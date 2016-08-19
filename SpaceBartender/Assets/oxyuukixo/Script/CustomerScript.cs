628bf8f62b35f230517f3f829bd8c16e42628a69
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-08-19 21:27:53 +0900

    客の出現から帰るまで 時間で空いてる場所にランダムで出現します 位置に対応した数字キーで飲み物が完成（全部喜びます） 一定時間経過したら怒って帰ります

---
+++ SpaceBartender/Assets/oxyuukixo/Script/CustomerScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerScript : MonoBehaviour {
+
+    public float RemoveTime;       //帰るまでの時間
+
+    private float ElapsedTime;      //経過時間
+
+    private Animator anim;          //アニメーション
+
+    // Use this for initialization
+    void Start () {
+
+        ElapsedTime = 0;
+
+        anim = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if((ElapsedTime += Time.deltaTime) > RemoveTime)
+        {
+            anim.SetBool("IsAngry", true);
+
+            Destroy(this.gameObject, 2);
+        }
+    }
+
+    //帰るまでの時間を回復する処理
+    //
+    //time  回復する時間
+    //
+    public void RecoveryTime(float time)
+    {
+        ElapsedTime -= time;
+    }
+
+    //飲み物が完成した時の処理
+    public void Drinking(){
+
+        anim.SetBool("IsHappy", true);
+
+        Destroy(this.gameObject, 2);
+    }
+}
